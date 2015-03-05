b05ceff10e719aa76f7e390d903979bbfe599152
pofider <honza.pofider@seznam.cz>
2015-03-05 21:25:29 +0100

    travis integration and remove q

--- test/test.js
+++ test/test.js
@@ -4,33 +4,44 @@ var should = require("should"),
 
 describe("phantom workers", function () {
 
+    var phantomManager;
+
     it("should be able to communicate with phantom", function (done) {
-        var phantomManager = new PhantomManager({
+        phantomManager = new PhantomManager({
             pathToPhantomScript: path.join(__dirname, "test-script", "script.js"),
             numberOfWorkers: 1
         });
-        phantomManager.start().then(function () {
-            return phantomManager.execute({foo: "test"}).then(function (res) {
+        phantomManager.start(function(err) {
+            if (err)
+                return done(err);
+
+            phantomManager.execute({foo: "test"}, function (err, res) {
+                if (err)
+                    return done(err);
+
                 res.should.have.property("foo");
                 res.foo.should.be.eql("test");
                 done();
-            })
-        }).catch(done);
+            });
+        });
     });
 
     it("should spin up specified number of workers", function (done) {
-        var phantomManager = new PhantomManager({
+        phantomManager = new PhantomManager({
             pathToPhantomScript: path.join(__dirname, "test-script", "script.js"),
             numberOfWorkers: 5
         });
-        phantomManager.start().then(function () {
+        phantomManager.start(function (err) {
+            if (err)
+                return done(err);
+
             phantomManager._phantomInstances.should.have.length(5);
             done();
-        }).catch(done);
+        });
     });
 
     it("timeout should emit event", function (done) {
-        var phantomManager = new PhantomManager({
+        phantomManager = new PhantomManager({
             pathToPhantomScript: path.join(__dirname, "test-script", "timeout.js"),
             numberOfWorkers: 1,
             timeout: 10
@@ -38,27 +49,37 @@ describe("phantom workers", function () {
         phantomManager.on("timeout", function (workerInstance) {
             done();
         });
-        phantomManager.start().then(function () {
-            return phantomManager.execute({});
-        }).catch(done);
+        phantomManager.start(function (err) {
+            if (err)
+                return done(err);
+
+            phantomManager.execute({}, function(err) {
+                if (!err)
+                    return done(new Error("Should not execute successfully"));
+            });
+        });
+
         setTimeout(function () {
             done(new Error("worker was not recycled"));
         }, 500);
     });
 
-    it("timeout should fail promise", function (done) {
-        var phantomManager = new PhantomManager({
+    it("timeout should cb error", function (done) {
+        phantomManager = new PhantomManager({
             pathToPhantomScript: path.join(__dirname, "test-script", "timeout.js"),
             numberOfWorkers: 1,
             timeout: 10
         });
 
-        phantomManager.start().then(function () {
-            return phantomManager.execute({}).then(function () {
-                done(new Error("timeout should fail promise"));
-            }).catch(function () {
+        phantomManager.start(function (err) {
+            if (err)
+                return done(err);
+
+            return phantomManager.execute({}, function (err, res) {
+                if (!err)
+                    return done(new Error("timeout should fail promise"));
                 done();
-            })
-        }).catch(done);
+            });
+        });
     });
 });
