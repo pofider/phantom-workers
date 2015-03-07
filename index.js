fb22a837d5f79509e36932973db84b4f6ef2750a
pofider <honza.pofider@seznam.cz>
2015-03-07 21:17:24 +0100

    fix initial bugs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 module.exports = function(options) {
-    return new require("./lib/phantomManager.js")(options);
+    return new (require("./lib/phantomManager.js"))(options);
 }
 
 module.exports.PhantomManager = require("./lib/phantomManager.js");
